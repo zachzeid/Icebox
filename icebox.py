5f22bf5dec0eb10e2a4a2f1d403ef8bc3216debb
Zachary Zeid <zach.zeid@AHD-MACAR13-013.local>
2015-08-21 09:55:46 -0400

    Added start/stop options, cleaned up some code

--- icebox.py
+++ icebox.py
@@ -3,39 +3,61 @@
 import boto
 import boto.ec2
 import time
+import argparse
+
+
 instance_list = []
 conn = boto.ec2.EC2Connection()
 region_list = conn.get_all_regions()
+
+parser = argparse.ArgumentParser(description='Start and stop instances')
+parser.add_argument('--stop', action="store_true", help="Stop instances", required=False)
+parser.add_argument('--start', action="store_true", help="Start instances", required=False)
+args = parser.parse_args()
+
 def region_connections(region_list):
 	conn_list = []
 	for region in region_list:
 		r_conn = boto.ec2.connect_to_region(region.name)
 		conn_list.append(r_conn)
-		#print region, r_conn, len(conn_list)
 	return conn_list
 
-def check_state(instanceList):
+
+def stop_state(instanceList):
 	instance_state = []
 	for list in instanceList:
 		for instance in list:
 			if instance.state == "running":
 				#print instance.state, instance.id
 				instance_state.append(instance.id)
-				instance.stop()
+				#instance.stop()
 				instance.update()
 	return instance_state
-def stop_instance(instanceId):
-	pass	
+
+
+def start_state(instanceList):
+	instance_state = []
+	for list in instanceList:
+		for instance in list:
+			if instance.state == "stopped":
+				instance_state.append(instance.id)
+				instance.start()
+				instance.update()
+	return instance_state
 
 
 conn_list = region_connections(region_list)
-
 for connection in conn_list:
 	instances = connection.get_all_instances()
 	for res in instances:
 		instance_list.append(res.instances)
 
-check = check_state(instance_list)
+if args.start:
+	start_instance = start_state(instance_list)
+	print "Starting instances", start_instance 
 
-for instance in check:
-	print instance
+if args.stop:
+	stop_instance = stop_state(instance_list)
+	print "Stopping instances", stop_instance
+#check = check_state(instance_list)
+
